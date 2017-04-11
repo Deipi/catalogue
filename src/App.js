8a3ee991ac21a2572e8c9f5fb55d3aa303d05f84
Yoli E Cald <yoxi29@hotmail.com>
2017-04-11 16:52:23 -0500

    src:  modified h2 in App

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <h2>Welcome to Git </h2>
         </div>
         <p className="App-intro">
           Hola Mundo
