96d46a6c81494f00c0c44e498c9bcea54416e619
ChrisAlchemist <cristian.perez.garcia.54@gmail.com>
2017-04-17 16:46:24 -0500

    rendering products-form

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import immutable from 'immutable';
+
+import { combineReducers } from 'redux-immutable';
+
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import { Provider } from 'react-redux';
+import { reducer as formReducer } from 'redux-form/immutable'
+
 import App from './App';
+
+
 import './index.css';
 
+import employesReducer from './reducers/products';
+
+const initialState = immutable.Map();
+
+const rootReducer = combineReducers({
+	form: formReducer,
+	products: employesReducer,
+});
+
+const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <App />,
+	<Provider store={ store }>
+  		<App />
+  	</Provider>,
   document.getElementById('root')
 );
