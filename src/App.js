6aab698da0721bedefc8f5b08b25441210a29d4c
ChrisAlchemist <cristian.perez.garcia.54@gmail.com>
2017-04-18 14:00:45 -0500

    header added to App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import ProductsForm from './containers/products-form';
 class App extends Component {
   render() {
     return (
-      <div className="App">        
+       <div className="App">
+        <div className="App-header">
+            <img src="http://www.deipi.com/images/logo.svg" width="200" heigth="200" alt="logo" />                            
+          <h2>Bienvenido a CPR Soluciones</h2>
+        </div>        
         <ProductsForm />     
       </div>
     );
