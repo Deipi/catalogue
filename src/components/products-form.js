76456ce8a1fa063175f1e0881a62274747d7b464
ChrisAlchemist <cristian.perez.garcia.54@gmail.com>
2017-04-20 15:43:18 -0500

    form updated

--- src/components/products-form.js
+++ src/components/products-form.js
@@ -1,101 +1,74 @@
 import React, { Component } from 'react';
 import { Field, FieldArray, reduxForm } from 'redux-form/immutable'
 import validate from './validate'
-import 'bootstrap/dist/css/bootstrap.css';
+
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
-
-
+import { Card, CardHeader, CardBlock, Button, CardTitle, CardText } from 'reactstrap';
 
 const renderField = ({ input, label, type, meta: { touched, error } }) => (
   <div>
     <label>{label}</label>
     <div>
-      <input {...input} type={type} placeholder={label} />
+      <input {...input} id="inputs" type={type} placeholder={label} />
       {touched && error && <span>{error}</span>}
     </div>
   </div>
 );
 
-
-const renderMembers = ({ fields, meta: { touched, error, submitFailed } }) => (
-  <ul>
-    <li>
-      <button type="button" onClick={() => fields.push({})}>Agregar Variante</button>
-      {(touched || submitFailed) && error && <span>{error}</span>}
-    </li>
-    {fields.map((variants, index) => (
-      <li key={index}>
-      <h4>Variante #{index + 1}</h4>
-        <button
-          type="button"          
-          onClick={() => fields.remove(index)}        > Eliminar</button>
-        
-        <Field
-          name={`${variants}.size`}
-          type="text"
-          component={Size}
-          label="Tamaño"
-        />
-
-        <Field
-          name={`${variants}.color`}
-          type="text"
-          component={Color}
-          label="Color"
-        />
-       
-      </li>
-    ))}
-  </ul>
+const renderSubProducts = ({ fields, meta: { touched, error, submitFailed } }) => (
+    <ul >
+        <div style={{ float: 'left', background: '#f0f0' }}>
+            <li>
+                <Button type="button" onClick={() => fields.push({})}>Crear</Button>
+                {(touched || submitFailed) && error && <span>{error}</span>}
+            </li>
+        </div>
+        <div style={{ float: 'right'}}>
+            <Card>
+             <CardHeader>Subproductos</CardHeader>
+                {fields.map((variants, index) => (
+                    <div>
+                        <Card block>
+                            <li key={index}>
+                                <h4>Variante #{index + 1}</h4>
+                                <Field name={`${variants}.size`} type="text" component={Size} label="Tamaño"/>
+                                <Button type="button" onClick={() => fields.remove(index)}>Eliminar</Button>
+                            </li>
+                        </Card>
+                    </div>
+                ))}
+            </Card>
+        </div>
+    </ul>
 );
 
 const NewProductForm = props => {
   const { handleSubmit, actionSubmit, pristine, reset, submitting } = props;
-  console.log('HOLA MUNDO')
-  console.log(handleSubmit);    
-  console.log(actionSubmit);    
   return (
-    <form onSubmit={ handleSubmit(actionSubmit) }>
-      <Field
-        name="name"
-        type="text"
-        component={renderField}
-        label="Nombre del producto"
-      />
-      <Field
-        name="code"
-        type="number"
-        component={renderField}
-        label="Código del producto"
-      />
-
-      <Field
-        name="price"
-        type="number"
-        component={renderField}
-        label="Precio del producto"
-      />
-      <Field
-        name="description"
-        type="text"
-        component="textarea"
-        placeholder="Descripción"
-      />
-
-      <Field name="tags" component={ Tags } type="text" placeholder="Tags"/>
-      <FieldArray name="variants" component={renderMembers} />
-      <div>
-        <button type="submit" disabled={submitting}>Guardar</button>
-        <button type="button" disabled={pristine || submitting} onClick={reset}>
-          Clear Values
-        </button>
-      </div>
-    </form>
+    <div>
+        <form onSubmit={ handleSubmit(actionSubmit) }>
+            <div style={{ float: 'left'}}>
+                <Field name="name" type="text" component={ renderField } label="Nombre del producto"/>
+                <Field name="code" type="number" component={renderField} label="Código del producto"/>
+                <Field name="price" type="number" component={renderField} label="Precio del producto"/>
+                <label>Descripción</label><br/>
+                <Field name="description" type="text" component="textarea" placeholder="Descripción"/>
+                <Field name="tags" component={ Tags } type="text" placeholder="Tags"/>
+                <label>Variantes</label>
+                <Field name="variantsSelect" type="input" component={Variants} />
+            </div>
+                <div style={{ float: 'left'}}>
+                    <FieldArray name="variants" component={renderSubProducts} />
+                </div>
+            <div style={{float:'left'}}>
+                <Button type="submit" disabled={submitting}>Guardar</Button>
+                <Button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</Button>
+            </div>
+        </form>
+    </div>
   );
 };
-
-
 
 class Tags extends Component{
     constructor(props) {
@@ -183,13 +156,13 @@ class Size extends  Component{
     const { multi, multiValue, options, value } = this.state;
     return (
       <div >
-
         <h3 >{this.props.label}</h3>
         <Select.Creatable
           multi={multi}
           options={options}
           onChange={this.handleOnChange}
           value={multi ? multiValue : value}
+          placeholder="Variantes"
         />
         <div >{this.props.hint}</div>
         
@@ -198,27 +171,29 @@ class Size extends  Component{
   }
 }
 
-var Color = React.createClass({
-  displayName: 'Color',
+class Size extends Component{
+    constructor(props) {
+        super(props);
+
+        this.handleOnChange = this.handleOnChange.bind(this);
+        this.state = {
+            displayName: 'Size',
+            multi: false,
+            multiValue: [],
+            options: [
+                { value: 'C', label: 'Chico' },
+                { value: 'M', label: 'Mediano' },
+                { value: 'G', label: 'Grande' }
+            ],
+            value: undefined
+        };
+    }
+
   propTypes: {
     hint: React.PropTypes.string,
     label: React.PropTypes.string
-  },
-  getInitialState () {
-    return {
-      multi: false,
-      multiValue: [],
-      options: [
-        { value: 'V', label: 'Verde' },
-        { value: 'A', label: 'Azul' },
-        { value: 'N', label: 'Negro' },
-        { value: 'G', label: 'Gris' },
-        { value: 'B', label: 'Blanco' },
-        { value: 'R', label: 'Rojo' }
-      ],
-      value: undefined
-    };
-  },
+  }
+
   handleOnChange (value) {
     const { multi } = this.state;
     if (multi) {
@@ -226,7 +201,8 @@ var Color = React.createClass({
     } else {
       this.setState({ value });
     }
-  },
+  }
+
   render () {
     const { multi, multiValue, options, value } = this.state;
     return (
@@ -237,19 +213,19 @@ var Color = React.createClass({
           multi={multi}
           options={options}
           onChange={this.handleOnChange}
+          placeholder="Tamaño"
           value={multi ? multiValue : value}
         />
+
         <div >{this.props.hint}</div>
-        
       </div>
     );
   }
-});
-
+}
 
 
 export default reduxForm({
   form: 'fieldArrays', // a unique identifier for this form
-  
+  validate,
 })(NewProductForm);
 
