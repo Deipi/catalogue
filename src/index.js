8f3b4739039b4c50c9ab6576ce89c3f72849f00d
ChrisAlchemist <cristian.perez.garcia.54@gmail.com>
2017-04-20 15:52:10 -0500

    new library added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import 'bootstrap/dist/css/bootstrap.css'
 import immutable from 'immutable';
 
 import { combineReducers } from 'redux-immutable';
