953f522b71fecda77d723bb0c646dffaf2d5d37c
ChrisAlchemist <cristian.perez.garcia.54@gmail.com>
2017-05-04 10:19:20 -0500

    calling the index links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ProductsForm from './containers/products-form';
+import {
+  Link
+} from 'react-router-dom'
 
 
 class App extends Component {
   render() {
+    const { props: { children } } = this;
     return (
-       <div className="App">
+      <div className="App">
         <div className="App-header">
-            <img src="http://www.deipi.com/images/logo.svg" width="200" heigth="200" alt="logo" />                            
+            <Link to="/">  <img src="http://www.deipi.com/images/logo.svg" width="200" heigth="200" alt="logo" /></Link>
           <h2>Bienvenido a CPR Soluciones</h2>
-        </div>        
-        <ProductsForm />     
+        </div>
+        { children }
+
       </div>
     );
   }
