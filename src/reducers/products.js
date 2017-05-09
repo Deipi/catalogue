15ca8f9dc3f15f6433796f61fc6909e42fd2aa6b
ChrisAlchemist <cristian.perez.garcia.54@gmail.com>
2017-05-09 14:12:54 -0500

    Products Fetch validated

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,10 +1,12 @@
 import { fromJS } from 'immutable';
-import { POSTED_PRODUCT } from '../actions';
+import { POSTED_PRODUCT, FETCHED_PRODUCTS } from '../actions';
 
 export default (state=fromJS([]), action) => {
 	switch(action.type) {
 		case POSTED_PRODUCT:
 			return state.merge(fromJS([action.payload]));
+		case FETCHED_PRODUCTS:
+			return fromJS(action.payload);
 		default:
 			return state;
 	}
