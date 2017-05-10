d1babbde77dd5587b73c2ea120ef2c81acdfb5b1
ChrisAlchemist <cristian.perez.garcia.54@gmail.com>
2017-05-10 09:19:46 -0500

    added the 'GET'that the list will use

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 
 export const POSTED_PRODUCT = 'POSTED_PRODUCT';
-
+export const FETCHED_PRODUCTS = 'FETCHED_PRODUCTS';
 
 export default data => (dispatch, getStore) => fetch('http://localhost:3004/products', {
 	method: 'POST',
@@ -11,4 +11,14 @@ export default data => (dispatch, getStore) => fetch('http://localhost:3004/prod
 }).then( result => result.json().then( product => dispatch({
 	type: POSTED_PRODUCT,
 	payload: product
+})));
+
+export const fetchProducts = () => (dispatch, getStore) => fetch('http://localhost:3004/products', {
+	method: 'GET',
+	headers: {
+		'Content-Type': 'application/json'
+	},
+}).then( result => result.json().then( products => dispatch({
+	type: FETCHED_PRODUCTS,
+	payload: products
 })));
