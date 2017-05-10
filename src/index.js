15ffe2696dff58fa181b9f3374c82313976eee2c
ChrisAlchemist <cristian.perez.garcia.54@gmail.com>
2017-05-10 15:54:15 -0500

    identation changed

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import InsertarProductos from './containers/products-form'
-import ListaProductos from './containers/products-list'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import immutable from 'immutable';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { combineReducers } from 'redux-immutable';
+import { createStore, applyMiddleware } from 'redux';
+import { reducer as formReducer } from 'redux-form/immutable';
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
-import DetailProducts from './containers/Details';
-import './Styles/src_IndexStyle.css'
 
-import immutable from 'immutable';
-import { reducer as formReducer } from 'redux-form/immutable';
-import { combineReducers } from 'redux-immutable';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import './Styles/src_IndexStyle.css'
 import employesReducer, { errorVariantReducer } from './reducers/products';
-
+import InsertarProductos from './containers/products-form'
+import ListaProductos from './containers/products-list'
+import DetailProducts from './containers/Details';
 
 const initialState = immutable.Map();
 
 const rootReducer = combineReducers({
 	form: formReducer,
 	products: employesReducer,
-  variantError: errorVariantReducer,
+	variantError: errorVariantReducer,
 });
 
 const routes = [
@@ -40,15 +39,15 @@ const routes = [
 
   { path: '/listado',
 	exact: true,
-	sidebar: () => <div>bubblegum!</div>,
+	sidebar: () => <div>list!</div>,
 	main: () => <ListaProductos />
   },
   { path: '/Nuevo Producto',
-	sidebar: () => <div>shoelaces!</div>,
+	sidebar: () => <div>New Product!</div>,
 	main: () => <InsertarProductos />
   },
   { path: '/Detalles/:name' ,
-	sidebar: () => <div>svdfuy!</div>,
+	sidebar: () => <div>Details!</div>,
 	main: DetailProducts,
   }
 ]
@@ -60,40 +59,41 @@ ReactDOM.render(
 	<Provider store={ store }>
 		<Router>
 			<App>
-			  <div style={{ display: 'flex' }}>
-				  <div id="Links">
-					<nav>
-						<ul className="Opciones">
-							<li className="anclaje">
-								<div className="Barra"></div>
-								<Link to="/" className="Menu">Inicio</Link>
-							</li>
+				<div style={{ display: 'flex' }}>
+					<div id="Links">
+						<nav>
+							<ul className="Opciones">
+								<li className="anclaje">
+									<div className="Barra"></div>
+									<Link to="/" className="Menu">Inicio</Link>
+								</li>
 
-							<li className="anclaje">
-								<div className="Barra"></div>
-								<Link to="/listado" className="Menu">Lista de Productos</Link>
-							</li>
-							<li className="anclaje">
-								<div className="Barra"></div>
-								<Link to="/Nuevo Producto" className="Menu">Nuevo Producto</Link>
-							</li>
-						</ul>
-					</nav>
-				  </div>
+								<li className="anclaje">
+									<div className="Barra"></div>
+									<Link to="/listado" className="Menu">Lista de Productos</Link>
+								</li>
+								<li className="anclaje">
+									<div className="Barra"></div>
+									<Link to="/Nuevo Producto" className="Menu">Nuevo Producto</Link>
+								</li>
+							</ul>
+						</nav>
+					</div>
 
-				  <div id="Contenido" >
-					{routes.map((route, index) => (
-					  <Route
-						key={index}
-						path={route.path}
-						exact={route.exact}
-						component={route.main}
-					  />
-					))}
-				  </div>
+					<div id="Contenido" >
+						{routes.map((route, index) => (
+								<Route
+									key={index}
+									path={route.path}
+									exact={route.exact}
+									component={route.main}
+								/>
+							)
+						)}
+					</div>
 				</div>
 			</App>
 		</Router>
 	</Provider>,
-  document.getElementById('root')
+	document.getElementById('root')
 );
