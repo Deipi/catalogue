cd72514848e3af45573d4c19c41a84cdaf64da7e
ChrisAlchemist <cristian.perez.garcia.54@gmail.com>
2017-05-12 14:20:07 -0500

    action POST Updated

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,16 +2,39 @@
 export const POSTED_PRODUCT = 'POSTED_PRODUCT';
 export const FETCHED_PRODUCTS = 'FETCHED_PRODUCTS';
 
-export default data => (dispatch, getStore) => fetch('http://localhost:3004/products', {
+// export default data => (dispatch, getStore) => fetch('http://localhost:3004/products', {
+// 	method: 'POST',
+// 	headers: {
+// 		'Content-Type': 'application/json'
+// 	},
+// 	body: JSON.stringify(data)
+// }).then( result => result.json().then( product => dispatch({
+// 	type: POSTED_PRODUCT,
+// 	payload: product
+// })));
+
+export default (data, subproducts) => (dispatch, getStore) => fetch('http://localhost:3004/products', {
 	method: 'POST',
 	headers: {
 		'Content-Type': 'application/json'
 	},
-	body: JSON.stringify(data)
-}).then( result => result.json().then( product => dispatch({
-	type: POSTED_PRODUCT,
-	payload: product
-})));
+	body: JSON.stringify(Object.assign({}, data, { parent: null }))
+}).then( result => result.json().then( product => {
+
+	subproducts.forEach(subProduct => fetch('http://localhost:3004/products', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(Object.assign({}, subProduct, { parent: product.id }))
+	}));
+
+	return dispatch({
+		type: POSTED_PRODUCT,
+		payload: product
+	})
+}));
+
 
 export const fetchProducts = () => (dispatch, getStore) => fetch('http://localhost:3004/products', {
 	method: 'GET',
