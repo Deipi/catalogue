de4a496749d1253cac38f4610de0750d0266ca65
ChrisAlchemist <cristian.perez.garcia.54@gmail.com>
2017-06-08 15:31:56 -0500

    paydolad updated

--- src/actions/index.js
+++ src/actions/index.js
@@ -86,7 +86,9 @@ export const deleteProduct = (id, subproducts) => (dispatch, getStore) => fetch(
 				}));
 				return dispatch({
 					type: DELETED_PRODUCT,
-					payload: product
+					payload: {
+						id
+					}
 					}
 				)
 			}
